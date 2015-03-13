ece97879d879b054a45880e7b786c97e203b565a
Brandon Silva <itbrandonsilva@gmail.com>
2015-03-13 18:33:15 -0400

    Added minimal frontend app.

---
+++ public/components/app.js
@@ -0,0 +1,34 @@
+angular.module('app', [])
+    .controller('AppController', function ($scope, $http) {
+
+        $scope.streams = [];
+
+        Twitch.init({clientId: '{{ client_id }}'}, function(err, status) {
+            if (err) return console.error(err);
+            Twitch.api({
+                method: 'streams',
+                params: {limit: 25} 
+            }, function (err, res) {
+                if (err) return console.error(err);
+                var streams = res.streams;
+                $scope.streams = streams;
+                streams.forEach(function (stream) {
+                    console.log("Name: %s, Viewers: %d", stream.channel.name, stream.viewers);
+                });
+                $scope.$apply();
+            });
+        });
+
+        $scope.setStream = function (channelName) {
+            console.log(channelName);
+            $http.get("/stream/set/" + channelName).then(
+                function (res) {
+                    console.log(res);
+                },
+                function (err) {
+                    console.error(err);
+                }
+            );
+        };
+    })
+;
--- server.js
+++ server.js
@@ -4,34 +4,40 @@ var express = require("express")
 var app = express()
 var spawn = require("child_process").spawn;
 var bodyParser = require("body-parser");
+var swig = require("swig");
 
 var PORT = 3000;
 var cmd = "/usr/local/bin/livestreamer";
+var config = require("./config.json");
 
+// Not currently needed.
 app.use(bodyParser.json());
 
 var ps = null;
 
-app.get("/", function (req, res) {
-    return res.send("Invalid request.");
+app.get("/components/app.js", function (req, res) {
+    res.append('content-type', 'text/javascript');
+    res.send(swig.renderFile("./public/components/app.js", {client_id: config.client_id}));
 });
 
-app.post("/", function (req, res) {
-    var url = req.body.url;
-    if ( ! url ) return res.send("Please provide a 'url' key in the post body.");
+app.get("/stream/set/:name", function (req, res) {
+    var channelName = req.params['name'];
+    res.send(channelName);
 
     if (ps) { ps.kill("SIGINT"); console.log("Killed player..."); }
     console.log("Spawning player...");
-    ps = spawn(cmd, [url, "best", "--player", "vlc"]);
+    var url = "twitch.tv/" + channelName;
+    ps = spawn(cmd, [url, "best", "-np", "\"omxplayer -o hdmi\""]);
+    //ps = spawn(cmd, [url, "best", "--player", "vlc"]);
     ps.on("error", function (err) {
         console.log(err.message);
     });
-    res.send(url);
-})
+});
 
+app.use(express.static('public'));
 
 var server = app.listen(PORT, function () {
-    var host = server.address().address
-    var port = server.address().port
+    var host = server.address().address;
+    var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port)
 })
